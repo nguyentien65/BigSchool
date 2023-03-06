393ec26d7eec95ee90e76c7cd9c888bae24799a2
Admin <Admin@VANTIEN>
2023-03-06 16:03:34 +0700

    Add project files.

---
+++ Models/ApplicationDbContextBase.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.Entity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace BigSchool.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public DbSet<Course> Course { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
